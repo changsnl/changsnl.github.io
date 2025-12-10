9d01facbaa18c143bbd4eeaf792f5f31540b1a69
park chang <allenjoyday@gmail.com>
2025-12-10 21:09:37 +0900

    add right gatenumber, add saveImage stack, add save line

--- allschedule/web/flutter_service_worker.js
+++ allschedule/web/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "e7e37edcbb70152e54067b95b0ca1843",
+"flutter_bootstrap.js": "42421ac9d75f91a02ef2a71e4a9708e1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "c98f8440975ab53f68202358b06bf99a",
 "/": "c98f8440975ab53f68202358b06bf99a",
-"main.dart.js": "85549f2e311fe93664179e89b8efcacb",
+"main.dart.js": "833b893e353294058d6495723c7ca154",
 "manifest.json": "c06efabda539d378a91fcd2e9221ea92",
 "version.json": "53f3820679e92dc5856aa26cdb4c3789"};
 // The application shell files that are downloaded before a service worker can
